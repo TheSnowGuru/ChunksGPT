2330cb1412a66dab27e20fc954fd36f5a36cfe72
Shay Ronen <shayronen@gmail.com>
2023-04-22 00:55:28 +0300

    Update code_divider.py

--- code_divider.py
+++ code_divider.py
@@ -1,38 +1,20 @@
 import re
 
-# Constants
-TOKEN_LIMIT = 8000
+def divide_codebase(codebase, module_size=8000):
+    """
+    Divide a codebase into smaller sections or modules that are within the 8K token limit.
+    """
+    modules = []
+    current_module = ''
+    current_size = 0
+    for line in codebase.split('\n'):
+        if current_size + len(line) > module_size:
+            modules.append(current_module)
+            current_module = ''
+            current_size = 0
+        current_module += line + '\n'
+        current_size += len(line) + 1
+    if current_size > 0:
+        modules.append(current_module)
 
-# Function to divide codebase into manageable sections
-def divide_codebase(codebase):
-    sections = []
-    current_section = ""
-    token_count = 0
-    
-    # Split codebase into lines
-    lines = codebase.split("\n")
-    
-    for line in lines:
-        # Check if line exceeds token limit
-        if len(line) > TOKEN_LIMIT:
-            print(f"Error: Line exceeds token limit ({len(line)} > {TOKEN_LIMIT})")
-            return None
-        
-        # Check if adding line will exceed token limit
-        if token_count + len(line) > TOKEN_LIMIT:
-            sections.append(current_section)
-            current_section = ""
-            token_count = 0
-        
-        current_section += line + "\n"
-        token_count += len(line)
-    
-    # Add last section to list
-    sections.append(current_section)
-    
-    return sections
-
-# Function to summarize code section
-def summarize_section(section):
-    # Split section into lines
-    lines = section.split
+    return modules
