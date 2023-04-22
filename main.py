1faec70766c07c189502a376594e9d5623bced33
Shay Ronen <shayronen@gmail.com>
2023-04-22 05:43:45 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,32 @@
-import timeit
-import pycallgraph
-from pycallgraph import PyCallGraph
-from pycallgraph.output import GraphvizOutput
-
+import os
 from code_divider import divide_codebase
-from code_summarizer import summarize_code
-from chatgpt_processor import process_code_with_chatgpt
+from chatgpt_processor import process_codebase_concurrently
 from results_integrator import integrate_results
-from validation_testing import validate_and_test
+from validation_testing import run_tests
 
-def main():
-    repo_url = "https://github.com/user/repo_name"
-    codebase = load_codebase_from_url(repo_url)
-    
-    # Time the iteration of the codebase
-    start_time = timeit.default_timer()
-    
-    modules = divide_codebase(codebase)
-    summaries = [summarize_code(module) for module in modules]
-    
-    processed_modules = [
-        process_code_with_chatgpt(module, summary)
-        for module, summary in zip(modules, summaries)
-    ]
-    
-    new_codebase = integrate_results(processed_modules)
-    
-    with open(f"{repo_url.split('/')[-1]}_suggestions.md", "w") as f:
-        f.write(new_codebase)
+# Load codebase from a GitHub repo URL
+repo_url = "https://github.com/your_username/your_repo"
+codebase = load_codebase_from_url(repo_url)
 
-    end_time = timeit.default_timer()
-    print(f"Time taken: {end_time - start_time} seconds")
+# Divide the codebase
+code_sections = divide_codebase(codebase)
 
-    # Generate codebase visualization
-    with PyCallGraph(output=GraphvizOutput()):
-        validate_and_test(new_codebase)
+# Process the codebase concurrently with ChatGPT
+chatgpt_responses = process_codebase_concurrently(code_sections)
 
-    # Save code structure in DOT language schema
-    pycallgraph.Config(include_stdlib=True)
-    graphviz = GraphvizOutput(output_file='code_structure.dot')
-    graphviz.done()
+# Integrate ChatGPT responses into the codebase
+modified_codebase = integrate_results(code_sections, chatgpt_responses)
 
-if __name__ == "__main__":
-    main()
+# Save the modified codebase to a new file
+with open("modified_codebase.py", "w") as f:
+    f.write(modified_codebase)
+
+# Run tests on the modified codebase
+test_results = run_tests("modified_codebase.py")
+print(test_results)
+
+# Print completion message and wait for user input
+print("Done digesting your repo! You can submit your feature request.")
+input("Press Enter to continue...")
+
+# You can also include the other necessary functions and steps as needed.
