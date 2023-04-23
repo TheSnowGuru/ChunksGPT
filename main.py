183617e552f860db0b61700cbea7d01cf9b45d4b
Shay Ronen <shayronen@gmail.com>
2023-04-23 14:15:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-from code_divider import divide_codebase
-from chatgpt_processor import process_codebase_concurrently
+import json
+from code_divider import divide_codebase, load_codebase_from_url
+from chatgpt_processor import process_codebase_concurrently, process_repo_insights
 from results_integrator import integrate_results
 from validation_testing import run_tests
 
@@ -17,13 +18,21 @@ chatgpt_responses = process_codebase_concurrently(code_sections)
 # Integrate ChatGPT responses into the codebase
 modified_codebase = integrate_results(code_sections, chatgpt_responses)
 
-# Save the modified codebase to a new file
-with open("modified_codebase.py", "w") as f:
-    f.write(modified_codebase)
+# Save the modified codebase to a new JSON file
+with open("modified_codebase.json", "w") as f:
+    json.dump(modified_codebase, f, indent=4)
 
 # Run tests on the modified codebase
-test_results = run_tests("modified_codebase.py")
+test_results = run_tests("modified_codebase.json")
 print(test_results)
+
+# Process repo insights
+repo_digest = 'Example repo digestion summary'  # Replace this with the actual digestion summary
+repo_insights = process_repo_insights(repo_digest)
+
+# Save the repo insights to a new JSON file
+with open("repo_insights.json", "w") as f:
+    json.dump(repo_insights, f, indent=4)
 
 # Print completion message and wait for user input
 print("Done digesting your repo! You can submit your feature request.")
